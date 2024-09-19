0b6555b90dae71aeefe1b3cb615abe6fa70f456c
vladkashka56 <vladkashka56@gmail.com>
2024-09-19 14:47:31 +0300

    add loading

--- src/App.js
+++ src/App.js
@@ -40,35 +40,26 @@ const _ = require('lodash');
 const CircularLoading = () => (
   <>
     <CircularProgress
-      size={70}
+      size={50}
       sx={{
         position: "fixed",
-        left: "50%",
-        top: "50%",
         transform: "translate(-50%, -50%)",
+        // display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
         zIndex: 2
       }}
       color='secondary'
     />
-    <DisabledBackground />
   </>
 );
 
-const DisabledBackground = styled(Box)({
-  width: "100%",
-  height: "100%",
-  position: "fixed",
-  background: "#ccc",
-  opacity: 0.5,
-  zIndex: 1
-});
+
 
 function App() {
 
   const [allTexts, setAllTexts] = useState({})
   const [templateName, setTemplateName] = useState("")
   const [newPresName, setNewPresName] = useState("")
-  const [newPresVideoName, setNewPresVideoName] = useState("")
+  const [newPresVideoName, setNewPresVideoName] = useState("updated_sample.mp4")
 
   const [notifyMessage, setNotifyMessage] = useState("")
   const [open, setOpen] = useState(false);
@@ -129,29 +120,28 @@ function App() {
 
 
   const getAllTexts = () => {
+    setLoading(true)
     Axios.post(`${API_HOST}/get/texts`, {pptFileName: templateName}, {
       headers: {
         'Content-Type': 'application/json'
       }
     })
      .then(response => {
+        setLoading(false)
         if(response.status === 200) {
           const allTextsArrayData = response.data.data
-
           const allTextsObjData = allTextsArrayData.reduce((acc, item, index) => {
             const textDatakey = item.slideIdx + "_" + item.shapeIdx
             acc[textDatakey] = item; // Use index as key
             return acc;
           }, {});
-
           setAllTexts(allTexts => ({ ...allTexts, ...allTextsObjData }));
-
           console.log("all texts:", allTextsObjData)
-
         }
         console.log(response);
       })
       .catch(error => {
+        setLoading(false)
         console.error(error);
       });
 
@@ -167,7 +157,7 @@ function App() {
 
 
   const updatePPTText = () => {
-    console.log("all texts:", allTexts)
+    setLoading(true)
     const allTextsArray = Object.values(allTexts);
     const textData = {
       template: templateName,
@@ -179,7 +169,8 @@ function App() {
         'Content-Type': 'application/json'
       }
     })
-     .then(response => {
+     .then(response => {  
+        setLoading(false)
         if(response.status === 200) {
           const responseData = response.data
           console.log("update response:", responseData)
@@ -190,6 +181,7 @@ function App() {
         console.log(response);
       })
       .catch(error => {
+        setLoading(false)
         console.error(error);
       });
   }
@@ -197,12 +189,14 @@ function App() {
 
 
   const exportVideo = () => {
+    setLoading(true)
     Axios.post(`${API_HOST}/template/export-video`, {pptFileName: newPresName}, {
       headers: {
           'Content-Type': 'application/json'
       }
     })
      .then(response => {
+        setLoading(false)
         if(response.status === 200) {
           const responseData = response.data
           console.log("export response:", responseData)
@@ -213,6 +207,7 @@ function App() {
         console.log(response);
       })
       .catch(error => {
+        setLoading(false)
         console.error(error);
       });
 
@@ -221,9 +216,8 @@ function App() {
 
 
   const handleDownload = async () => {
-
-    const config = {  
-      method: 'get',  
+    const config = { 
+      method: 'get', 
       url: `${API_HOST}/template/download?file_name=${newPresVideoName}`,  
       responseType: 'blob',  
       headers: {  
@@ -231,7 +225,7 @@ function App() {
         'Content-Type': 'application/json',  
       },  
     };  
-  
+
     try {  
       const response = await  Axios.request(config);  
       const blob = response.data;  
@@ -248,76 +242,37 @@ function App() {
   }
 
 
-
-
-
-  // const handleDownload = async () => {
-  //   const response = await Axios.post(`${API_HOST}/template/download`, {fileName: newPresVideoName}, {
-  //     headers: {
-  //         'Content-Type': 'application/json'
-  //     }
-  //   })
-
-  //   if (response.ok) {
-  //       const blob = await response.blob();
-  //       const url = window.URL.createObjectURL(blob);
-  //       const a = document.createElement('a');
-  //       a.href = url;
-  //       a.download = 'test.mp4'; // Specify the name for the downloaded file
-  //       document.body.appendChild(a);
-  //       a.click();
-  //       a.remove();
-  //   } else {
-  //       console.error('File download failed');
-  //   }
-
-
-  // }
-
-
-
-
-
-
-
   return (
     <Box sx={{ width : "100%" }}>
-      {loading ? <CircularLoading /> : null}
       <Grid spacing={4} container>
         <Grid xs={12} item sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
           <UploadButton handleUpload={handleFileUpload} />
         </Grid>
 
         <Grid xs={12} item sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        
+          {loading ? <CircularLoading /> : null}
+        
           <Grid xs={6} item>
             <Box sx={{ p: 2, border: '1px solid #000', height: '400px' }}> 
-            
               {Object.values(allTexts).map((textObj, index) => (
                 <TextInputField key={index} shapeIdx={textObj.shapeIdx} slideIdx={textObj.slideIdx} text={textObj.text} handleTextChange={handleTextChange} />
               ))}
-
             </Box>
-
-
             <Box sx={{marginTop: '10px', float: 'right'}}>
-              <Button variant="contained"  color="primary" sx={{marginLeft: '10px'}} onClick={getAllTexts}>
+              <Button variant="contained"  color="primary" sx={{marginLeft: '10px'}} onClick={getAllTexts} disabled={loading}>
                 Extract Text
               </Button>
-              <Button variant="contained"  color="success" sx={{marginLeft: '10px'}} onClick={updatePPTText}>
+              <Button variant="contained"  color="success" sx={{marginLeft: '10px'}} onClick={updatePPTText} disabled={loading}>
                 Update Text
               </Button>
-              <Button variant="contained" color="secondary" sx={{marginLeft: '10px'}} onClick={exportVideo}>
+              <Button variant="contained" color="secondary" sx={{marginLeft: '10px'}} onClick={exportVideo} disabled={loading}>
                 Create Video
               </Button>
-              <Button variant="contained" color="error" sx={{marginLeft: '10px'}} onClick={handleDownload}>
+              <Button variant="contained" color="error" sx={{marginLeft: '10px'}} onClick={handleDownload} disabled={loading}>
                 Download
               </Button>
-
-              
             </Box>
-
-
-
           </Grid>
         </Grid>
 
@@ -337,57 +292,6 @@ function App() {
             {notifyMessage}
           </Alert>
         </Snackbar>
-
-
-        {/* <Grid xs={6} item>
-          <Grid sx={{marginBottom: 4}} spacing={4} container>
-            <Grid xs={6} item>
-              <TextField fullWidth label="Row Count" variant='standard' type='number' defaultValue={4} value={rowCount} onChange={handleRowCount} />
-            </Grid>
-            <Grid xs={6} item>
-              <TextField fullWidth label="Col Count" variant='standard' type='number' defaultValue={5} value={colCount} onChange={handleColCount} />
-            </Grid>
-          </Grid>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-              <TableHead>
-                <TableRow>
-                  {
-                    Array.from({length: colCount}, (_, index) => (<TableCell key={index}>{wordList[index]}</TableCell>))
-                  }
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {
-                  Array.from({length: rowCount}, (_, index) => (
-                    <TableRow
-                      key={index}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      {Array.from({length: colCount}, (_, i) => (<TableCell key={`${index}-${i}`}>{rows[index][i]}</TableCell>))}
-                    </TableRow>
-                  ))
-                }
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-        <Grid xs={6} item>
-          <TextareaAutosize minRows={12} sx={{ width: "100%"}} value={description} onChange={handleDescription}/>
-        </Grid>
-        
-        <Box sx={{width: '100%', marginTop: 4,display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Box sx={{ marginRight: 4}}>
-            <ColorPicker value={bgColor} onChange={handleFontColor} />
-          </Box>
-          <UploadButton handleUpload={handleFileUpload} />
-          <LinearProgress sx={{ width: 200, marginLeft: 4}} variant="determinate" value={uploadProgress} />
-        </Box>
-        <Box sx={{width: '100%', marginTop: 4,display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Button variant="contained" endIcon={<DownloadIcon />} onClick={handleDownload}>
-            Download
-          </Button>
-        </Box> */}
       </Grid>
     </Box>
   );
--- src/components/TextInputField.jsx
+++ src/components/TextInputField.jsx
@@ -19,23 +19,23 @@ import {
 
 const TextInputField = ({shapeIdx, slideIdx, text, handleTextChange}) => {
 
-  const [value, setValue] = useState(text)
+  const [value, setValue] = useState(text || '')
+
+  useEffect(() => {
+    setValue(text);
+  }, [text]);
 
   const handleChange = (e) => {
     const newTextValue = e.target.value
     const textDataKey = slideIdx + "_" + shapeIdx
     const textData = {shapeIdx, slideIdx, text: newTextValue}
     console.log("updated text data:", textDataKey, textData)
-
     handleTextChange({ idx: textDataKey, newValue: textData})
   }
 
-  // slideIdx: 269, shapeIdx: 5, text: 'Body Text Goes here '
-
-
   return (
     <Box sx={{ width: '100%', padding: '5px'}}>
-      <TextField multiline fullWidth defaultValue={value} onChange={handleChange} />
+      <TextField multiline fullWidth value={value} onChange={handleChange} />
     </Box>
   );
 };
